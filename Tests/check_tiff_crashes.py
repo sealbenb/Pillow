cbfb6565652749fce2993c1e80ef8f6a4627555e
Ben Bader <ben.b@sealsecurity.io>
2024-08-13 18:14:45 +0300

    CVE-2021-25289.patch

--- Tests/check_tiff_crashes.py
+++ Tests/check_tiff_crashes.py
@@ -20,6 +20,8 @@ repro_read_strip = (
     "images/crash_1.tif",
     "images/crash_2.tif",
     "Tests/images/crash-2020-10-test.tif",
+    "Tests/images/crash-1152ec2d1a1a71395b6f2ce6721c38924d025bf3.tif",
+    "Tests/images/crash-0e16d3bfb83be87356d026d66919deaefca44dac.tif",
 )
 
 for path in repro_read_strip:
